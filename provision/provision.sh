76ab9a30979c468062f6a1018f47895f591f3474
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2014-07-06 14:49:56 +0900

    avoid "ERROR: provided hosts list is empty"

--- provision/provision.sh
+++ provision/provision.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 set -e
 apt-get install -y ansible
+# avoid "ERROR: provided hosts list is empty"
+grep -q localhost /etc/ansible/hosts || cat >/etc/ansible/hosts <<EOF
+
+[local]
+localhost
+EOF
 ansible-playbook /vagrant/provision/site.yml --connection=local
