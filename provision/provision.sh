a0da3c913de709728d067c6f919788229fe479dd
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2014-07-06 14:53:34 +0900

    run `ansible-galaxy install` inside

--- provision/provision.sh
+++ provision/provision.sh
@@ -7,4 +7,5 @@ grep -q localhost /etc/ansible/hosts || cat >/etc/ansible/hosts <<EOF
 [local]
 localhost
 EOF
+[ -d /etc/ansible/roles ] || ansible-galaxy install -r /vagrant/provision/roles/Rolefile
 ansible-playbook /vagrant/provision/site.yml --connection=local
